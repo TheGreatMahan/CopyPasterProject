1b043c2de35aa80cf1af5fbc0336419472755781
Michael Ivanov <m_ivanov82275@fanshaweonline.ca>
2022-10-16 18:49:21 -0400

    created login & register page skeleton

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,123 @@
+import React, { useState, useReducer } from "react";
+import { ThemeProvider } from "@mui/material/styles";
+// import Logo from "./worldimage.png"
+import {
+    AppBar,
+    Toolbar,
+    Card,
+    CardHeader,
+    CardContent,
+    Typography,
+    Button,
+    TextField,
+
+} from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+import theme from "../theme";
+import "../App.css";
+
+
+
+const Home = () => {
+
+    const initialState = {
+        handleUsername: "",
+        handlePassword: "",
+    };
+
+    const reducer = (state, newState) => ({ ...state, ...newState });
+    const [state, setState] = useReducer(reducer, initialState);
+
+    const handleUsernameFunction = (event) => { setState(state.handleUsername = event.target.value); }
+    const handlePasswordFunction = (event) => { setState(state.handlePassword = event.target.value); }
+
+    const navigate = useNavigate();
+
+    const registerPage = () => {
+        navigate("/register")
+    }
+
+    // TODO for backend developer : link up login button to backend
+
+    const handleLoginButton = async () => {
+        // try {
+        //     let query = JSON.stringify({
+        //         query: `mutation {addproject(teamName: "${state.teamName}", projectName: "${state.projectName}", projectStartDate: "${state.projectStartDate}", hoursEquivanlentToStoryPoint: ${state.numHoursForStoryPoint}, totalEstNumberOfStoryPoints: ${state.numEstimatedStoryPoints}, totalEstCostForDevelopment: ${state.estimatedCostForDevelopment}, sprintNumber: ${state.sprintNumber})
+        //                 { teamName, projectName, projectStartDate, hoursEquivanlentToStoryPoint, totalEstNumberOfStoryPoints, totalEstCostForDevelopment, sprintNumber }}`,
+        //     });
+
+        //     await fetch('http://localhost:5000/graphql', {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json; charset=utf-8",
+        //         },
+        //         body: query,
+        //     });
+        //     console.log(`save all textfield data mutation complete`);
+
+        // } catch (error) {
+        //     console.log(`error adding all textfield data mutation: ${error}`);
+        // }
+    }
+
+    const emptyorundefined =
+        state.handleUsername === "" || state.handleUsername == undefined ||
+        state.handlePassword === "" || state.handlePassword == undefined
+
+
+    return (
+        <ThemeProvider theme={theme}>
+
+            <Card style={{ textAlign: 'center' }}>
+
+
+                {/* add our own logo maybe ?  */}
+                {/* <img src={Logo} alt="Logo" style={{width:"50%",marginTop:70, marginLeft:100,marginBottom:-50}}/> */}
+                <CardHeader
+                    title="ProActinators"
+                    style={{ marginTop: 30 }}
+                />
+
+                <CardHeader
+                    title="Login here"
+                    style={{ marginTop: 50 }}
+                />
+
+                <Card style={{ boxShadow: "none" }} >
+                    <TextField
+                        style={{ marginTop: 20 }}
+                        label="Enter username"
+                        onChange={handleUsernameFunction}
+                    />
+                </Card>
+                <Card style={{ boxShadow: "none" }} >
+                    <TextField
+                        style={{ marginTop: 20, marginBottom: 20 }}
+                        label="Enter password"
+                        onChange={handlePasswordFunction}
+                    />
+                </Card>
+
+
+                <Button
+                    disabled={emptyorundefined}
+                    color="secondary" variant="contained" onClick={handleLoginButton}>
+                    Login
+                </Button>
+
+                <CardHeader
+                    title="Not registered?"
+                    style={{ marginTop: 60 }}
+                />
+
+                <Button style={{ marginBottom: 30 }} color="secondary" variant="contained" onClick={registerPage}>
+                    Register here
+                </Button>
+
+
+
+            </Card>
+        </ThemeProvider>
+    );
+};
+export default Home;
---
+++ frontend/src/Components/Register.js
@@ -0,0 +1,107 @@
+import React, { useState, useReducer } from "react";
+import { ThemeProvider } from "@mui/material/styles";
+// import Logo from "./worldimage.png"
+import {
+    AppBar,
+    Toolbar,
+    Card,
+    CardHeader,
+    CardContent,
+    Typography,
+    Button,
+    TextField,
+
+} from "@mui/material";
+import theme from "../theme";
+import "../App.css";
+
+
+
+const Register = () => {
+
+    const initialState = {
+        handleUsername: "",
+        handlePassword: "",
+    };
+
+    const reducer = (state, newState) => ({ ...state, ...newState });
+    const [state, setState] = useReducer(reducer, initialState);
+
+    const handleUsernameFunction = (event) => { setState(state.handleUsername = event.target.value); }
+    const handlePasswordFunction = (event) => { setState(state.handlePassword = event.target.value); }
+
+
+
+    // TODO for backend developer : link up register button to backend
+
+    const handleRegisterButton = async () => {
+        // try {
+        //     let query = JSON.stringify({
+        //         query: `mutation {addproject(teamName: "${state.teamName}", projectName: "${state.projectName}", projectStartDate: "${state.projectStartDate}", hoursEquivanlentToStoryPoint: ${state.numHoursForStoryPoint}, totalEstNumberOfStoryPoints: ${state.numEstimatedStoryPoints}, totalEstCostForDevelopment: ${state.estimatedCostForDevelopment}, sprintNumber: ${state.sprintNumber})
+        //                 { teamName, projectName, projectStartDate, hoursEquivanlentToStoryPoint, totalEstNumberOfStoryPoints, totalEstCostForDevelopment, sprintNumber }}`,
+        //     });
+
+        //     await fetch('http://localhost:5000/graphql', {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json; charset=utf-8",
+        //         },
+        //         body: query,
+        //     });
+        //     console.log(`save all textfield data mutation complete`);
+
+        // } catch (error) {
+        //     console.log(`error adding all textfield data mutation: ${error}`);
+        // }
+    }
+
+    const emptyorundefined =
+        state.handleUsername === "" || state.handleUsername == undefined ||
+        state.handlePassword === "" || state.handlePassword == undefined
+
+    return (
+        <ThemeProvider theme={theme}>
+
+            <Card style={{ textAlign: 'center' }}>
+
+
+                {/* add our own logo maybe ?  */}
+                {/* <img src={Logo} alt="Logo" style={{width:"50%",marginTop:70, marginLeft:100,marginBottom:-50}}/> */}
+                <CardHeader
+                    title="ProActinators"
+                    style={{ textAlign: "center", marginTop: 30 }}
+                />
+
+                <CardHeader
+                    title="Register"
+                    style={{ textAlign: "center", marginTop: 50 }}
+                />
+
+                <Card style={{ boxShadow: "none" }} >
+                    <TextField
+                        style={{ marginTop: 20 }}
+                        label="Enter username"
+                        onChange={handleUsernameFunction}
+                    />
+                </Card>
+                <Card style={{ boxShadow: "none" }} >
+                    <TextField
+                        style={{ marginTop: 20 }}
+                        label="Enter password"
+                        onChange={handlePasswordFunction}
+                    />
+                </Card>
+
+
+                <Button
+                    disabled={emptyorundefined}
+                    style={{ marginBottom: 30, marginTop: 30 }} color="secondary" variant="contained" onClick={handleRegisterButton}>
+                    Register
+                </Button>
+
+
+            </Card>
+        </ThemeProvider>
+    );
+};
+export default Register;
