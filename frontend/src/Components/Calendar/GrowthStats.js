ca666f5969b2e20634759416bf30b570f4ea213a
golbahar-mozun <80173389+golbahar-mozun@users.noreply.github.com>
2022-12-19 00:48:20 -0500

    added friend stats

--- frontend/src/Components/Calendar/GrowthStats.js
+++ frontend/src/Components/Calendar/GrowthStats.js
@@ -8,6 +8,9 @@ import {
     CardContent,
     Box,
     Typography,
+    TextField,
+    Button,
+    Autocomplete
 } from "@mui/material";
 import { useAuth } from "../Auth";
 
@@ -21,7 +24,11 @@ const GrowthStats = () => {
         chartDataY: [],
         totalPoints: 0,
         losses: 0,
-        wins: 0
+        wins: 0,
+        currentFriend: "",
+        friends: [],
+        users: [],
+        friendStats: []
     };
 
     const reducer = (state, newState) => ({ ...state, ...newState });
@@ -29,8 +36,9 @@ const GrowthStats = () => {
 
 
     useEffect(() => {
-        fetchTasksForUser(auth.user)
-    }, [])
+        fetchTasksForUser(auth.user);
+        fetchPotentialFriends();
+    }, []);
 
     const GRAPHURL = "http://localhost:5000/graphql";
 
@@ -48,13 +56,49 @@ const GrowthStats = () => {
 
             let payload = await response.json();
 
-            let labels = payload.data.tasksforuser.filter((data) => data.completiondate !== null && data.completiondate !== "");
+            let stats = calculateStats(payload.data.tasksforuser);
+
+            if(user === auth.user)
+            {
+               setState({chartDataX: stats.chartDataX, chartDataY: stats.chartDataY, totalPoints: stats.totalPoints, losses: stats.losses, wins: stats.wins}); 
+            }
+            
+            return payload.data.tasksforuser;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const fetchPotentialFriends = async () => {
+        try {
+            let response = await fetch(GRAPHURL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                },
+                body: JSON.stringify({
+                    query: `query {users{username}}`,
+                }),
+            });
+
+            let payload = await response.json();
+            let data = payload.data.users.filter((user) => user !== auth.user && state.friends.indexOf(user) === -1);
+            setState({users: data});
+
+            return payload.data.tasksforuser;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const calculateStats = (stats) => {
+            let labels = stats.filter((data) => data.completiondate !== null && data.completiondate !== "");
             labels = labels.map((data) => data.completiondate);
             labels = labels.map((data) => new Date(data));
             labels = labels.map((data) => data.toLocaleDateString());
             labels = labels.filter((v, i, a) => a.indexOf(v) === i);
 
-            let dateData = payload.data.tasksforuser
+            let dateData = stats
             dateData = dateData.filter((data) => data.completiondate !== null && data.completiondate !== "");
             dateData.forEach((data) => { data.completiondate = new Date(data.completiondate) });
             dateData.forEach((data) => { data.completiondate = data.completiondate.toLocaleDateString() });
@@ -80,16 +124,42 @@ const GrowthStats = () => {
             let wins = numValues[2].points;
 
 
-            setState({ chartDataX: x, chartDataY: y, totalPoints: totalPoints, losses: losses, wins: wins });
-            console.log('x: ' + x)
-            console.log('y: ' + y)
+            return { chartDataX: x, chartDataY: y, totalPoints: totalPoints, losses: losses, wins: wins };
 
-            return payload.data.tasksforuser;
-        } catch (error) {
-            console.log(error);
-        }
     };
 
+    const onChangeFriendSelect = (e, selectedOption) => {
+        selectedOption
+          ? setState({ currentFriend: selectedOption })
+          : setState({ currentFriend: "" });
+    
+        // if (
+        //   state.Subject === "" ||
+        //   state.selectedUser === null ||
+        //   state.StartTime === "" ||
+        //   state.Description === "" ||
+        //   state.difficulty === -1 ||
+        //   state.priority === -1
+        // ) {
+        //   setButtonDisabled(true);
+        // } else {
+        //   setButtonDisabled(false);
+        // }
+      };
+
+      const buttonPressAddFriend = async () => {
+        if(state.currentFriend !== auth.user && state.friends.indexOf(state.currentFriend) === -1 )
+        {
+            let friends = state.friends.concat(state.currentFriend);
+            setState({friends: friends});
+            friends.forEach(async friend => {
+                let data = await fetchTasksForUser(friend.username);
+                let stats = calculateStats(data);
+                stats.username = friend.username
+                setState({friendStats: state.friendStats.concat(stats)});
+            });
+        } 
+      };
 
     return (
         <Box textAlign='center'>
@@ -98,7 +168,43 @@ const GrowthStats = () => {
                     title="Your points displayed"
                     style={{ color: 'black', textAlign: "center" }}
                 />
-
+                <div style={{position: "relative"}}>
+                <Autocomplete
+              data-testid="autocomplete"
+              options={state.users.map((usr) => {
+                return usr;
+              })}
+              getOptionLabel={(option) => option.username}
+              style={{ width: "20%", left: "10%" }}
+              onChange={onChangeFriendSelect}
+              value={state.currentFriend}
+              renderInput={(params) => (
+                <TextField
+                  style={{ marginTop: 5 }}
+                  {...params}
+                  label="Users"
+                  variant="outlined"
+                  fullWidth
+                />
+              )}
+            />
+                <Button
+              style={{
+                marginRight: 50,
+                //position: "absolute",
+                left: "10px",
+                display: "block",
+                marginTop: "30px"
+              }}
+              variant="contained"
+              onClick={() => {
+                buttonPressAddFriend();
+              }}
+            >
+              Add Friend
+            </Button>
+            <Typography dangerouslySetInnerHTML={{__html: `Your friends ==&gt; ${state.friendStats.map((stat) => {return " <strong> " + stat.username + "</strong>" + ": " + stat.totalPoints + "points"}) }`}}></Typography>
+                </div>
                 <Typography style={{ fontWeight: '500', fontSize: '24px' }}>Total points: {state.totalPoints}</Typography>
                 <Typography style={{ fontWeight: '450', color: 'green' }}>Wins: {state.wins}</Typography>
                 <Typography style={{ fontWeight: '450', color: 'red' }}>Losses: {state.losses}</Typography>
